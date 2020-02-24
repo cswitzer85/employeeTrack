4f5e52a8b3f6c7b837fafd34550deaa4a786739c
Cody Switzer <cswitzer85@gmail.com>
2020-02-24 13:01:38 -0600

    created schema and added other empty files

---
+++ schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS cliDB;
+CREATE database cliDB;
+USE cliDB;
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name varchar(30), 
+    last_name, varchar(30),
+    role_id, int, FK,
+    manager_id, int, FK;
+      PRIMARY KEY (id)
+
+    );
+
+  CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title, varchar(30),
+    salary, decimal,
+    department_id, int, FK
+    PRIMARY KEY (id)
+
+    );
+
+  CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name, varchar(30)
+    PRIMARY KEY (id)
+
+  );
+
+
+SELECT * FROM cliDB;
+
+
