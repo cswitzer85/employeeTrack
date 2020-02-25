3d2fcdc03bd4d64f35988bb8686cceb946b9da59
Cody Switzer <cswitzer85@gmail.com>
2020-02-24 20:24:27 -0600

    updated schema

--- schema.sql
+++ schema.sql
@@ -9,7 +9,6 @@ CREATE TABLE employee (
     role_id, int, FK,
     manager_id, int, FK;
     PRIMARY KEY (id)
-
     );
 
   CREATE TABLE role (
@@ -18,15 +17,37 @@ CREATE TABLE employee (
     salary, decimal,
     department_id, int, FK
     PRIMARY KEY (id)
-
     );
 
-  CREATE TABLE department (
+  CREATE TABLE department ( 
     id INT NOT NULL AUTO_INCREMENT,
     name, varchar(30)
     PRIMARY KEY (id)
-
   );
+
+insert into employee (first_name, last_name, role_id, manager_id)
+    values  ("John", "Doe", 1, 1),
+            ("Jane", "Doe", 2, 2),
+            ("Don", "Ho", 2, 1),
+            ("Tarzan", "O'Jungle", 3, 4),
+            ("Jane", "O'Jungle", 4, 3),
+            ("Jane", "Jetson", 5, 5)
+
+insert into role (title, salary, department_id)
+    values  ("Pawn", 30000, 1),
+            ("Knight", 75000, 2),
+            ("Bishop", 60000, 4),
+            ("Tree Swinger", 20000, 3),
+            ("Entitled", 160000, 3),
+            ("Tech Officer", 58000, 2),
+
+insert into employee (name)
+    values  ("Mail Room"),
+            ("Logistics"),
+            ("Management"),
+            ("Accounting")
+
+
 
 
 SELECT * FROM cliDB;
