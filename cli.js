3131cc5b077d160adbdf15100f7a146b0d2dbc38
Cody Switzer <cswitzer85@gmail.com>
2020-02-24 21:06:47 -0600

    updated switch statements

--- cli.js
+++ cli.js
@@ -2,6 +2,16 @@ const mysql2 = require("mysql2/promise");
 const inquirer = require("inquirer");
 const consoleDotTable = require("console.table");
 
+const choiceOptions = [
+    "Create an employee",
+    "Create a role",
+    "Create a department",
+    "View employee",
+    "View role",
+    "View department",
+    "Update employee role"
+];
+
 try {
     cli();
 } catch (error) {
@@ -15,36 +25,69 @@ async function cli() {
        database: "company_db" 
     });
 
-    const {choice}  await inquirer.prompt([
-        {
-            type "list",
-            name: "firstQ",
-            message: "First?",
-            choices: ["1", "2", "3", "4"]
-        },
+    const {choice} = await inquirer.prompt([
         {
             type: "list",
-            name: "secondQ",
-            message: "What type of employee would you like to add?",
-            choices: ["1", "2", "3", "4"]
-        },
-        {
-            type "list",
-            name: "thirdQ",
+            name: "choice",
             message: "First?",
-            choices: ["1", "2", "3", "4"]
-        },
-        {
-            type "list",
-            name: "fourthQ",
-            message: "First?",
-            choices: ["1", "2", "3", "4"]
-        },
-                {
-            type "list",
-            name: "fifthQ",
-            message: "First?",
-            choices: ["1", "2", "3", "4"]
-        }
+            choices: choiceOptions
+    }
+    // }, << if uncommented, delete yellow curly bracket on line above >> ---------- ReAd BeFoRe UnCoMmEnTiNg! <<-----{{{ <<-----{{{
+        // {
+        //     type: "list",
+        //     name: "secondQ",
+        //     message: "What type of employee would you like to add?",
+        //     choices: ["1", "2", "3", "4"]
+        // }
     ])
+
+    switch (choice) {
+         
+        case "Create an employee": //first_name, last_name, role_id, manager_id
+            var {firstName} = await requestInput ("What is the new employee's first name", firstName);
+            var {lastName} = await requestInput ("What is the new employee's last name", lastName);
+            var {role_id} = await requestInput ("What is the new employee's role ID number", role_id);
+            var {manager_id} = await requestInput ("What is the new employee's manager ID number", manager_id);
+            console.log(first_name, last_name, role_id, manager_id);
+            cli();
+            break;
+
+        case "Create a role": //title, salary, department_id
+            var {title} = await requestInput ("What is the title for the new role?", title);
+            var {salary} = await requestInput ("What is the salary for the new role?", salary);
+            var {department_id} = await requestInput ("What is the department id number for the new role?", department_id);
+            console.log(title, salary, department_id);
+            cli();
+            break;
+
+        case "Create a department": //name
+            var {name} = await requestInput ("What is the name of the new department?", name);
+            console.log(name);
+            cli();
+            break;
+
+        // case "View employee":
+        //     console.log(employeeSomething);
+        //     cli();
+        //     break;
+
+        // case "View role":
+        //     console.log(roleSomething);
+        //     cli();
+        //     break;
+
+        // case "View department":
+        //     console.log(departmentSomething);
+        //     cli();
+        //     break;
+
+        // case "Update employee role":
+        //     console.log(differentThing);
+        //     cli();
+        //     break;
+
+    }
+
+
+
 }
