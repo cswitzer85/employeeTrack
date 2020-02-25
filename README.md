b21cc8de3b2305539e5f4a1b079f647fdbbe1510
Cody Switzer <cswitzer85@gmail.com>
2020-02-24 21:47:29 -0600

    updated readme, created gitignore and json files.

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # employeeTrack
 employeeTrack is a solution for managing a company's employees using node, inquirer, and MySQL.
+
+<!-- TABLE OF CONTENTS -->
+## Table of Contents
+
+* [About the Project](#About-the-project)
+* [Getting Started](#Getting-started)
+  * [Prerequisites](#Prerequisites)
+  * [Installation](#Installation)
+* [Usage](#Usage)
+* [Contributing](#Contributing)
+* [Contact](#Contact)
+* [License(s)](#License(s))
+* [Acknowledgements](#Acknowledgements)
+
+## About the Project
+takeNotes is a locally hosted project that allows the user to add, read and delete notes. The front end was defined by SMU Coding Boot Camp as were various other documents in order to require students of the Boot Camp to learn how to build development approaches based on factors that they have no control over.
+
+## Getting Started
+    Pre-requisites:
+        This project requires node modules, if these are not present, you will need to use `npm init` to retrieve them.
+        The cli.js file must be run in order to host the project locally, this file requires express and path.
+        Open takeNotes in your favorite code editor or terminal/console and make sure your commands will be associated with the correct file location.
+    Installation:
+        If you do not have node_modules included in your download, type the following into the terminal...
+            `npm init`
+        If you do not have mysql2, inquirer or console.table installed, type the following into the terminal...
+            `npm install mysql2 inquirer console.table`
+    Launch:
+        Once all the pre-requisites have been satisfied, type the following into the terminal...
+            `node cli.js`
+
+
+## Usage
+Once the program is initiated, a prompt will appear. Simply make selections and add input where requested to add, view or change the data that has been selected.
+<!-- ![alt text](pictureURL goes here) -->
+
+
+## Contributing
+This project was completed as an assignment for SMU Coding Boot Camp, and as such is not subject to any regular update or maintenance. However, if you would like to extend advise, inquire about the project or otherwise communicate about this project, please use any of the means in the contact section below.
+
+
+## Contact
+GitHub user name: cswitzer85
+Linked-In: cody-switzer-1429593a
+![alt text](https://www.linkedin.com/in/cody-switzer-1429593a/)
+e-mail: cswitzer85@gmail.com
+
+## License(s)
+* ISC
+
+<!-- ## Acknowledgements -->
--- cli.js
+++ cli.js
@@ -3,91 +3,104 @@ const inquirer = require("inquirer");
 const consoleDotTable = require("console.table");
 
 const choiceOptions = [
-    "Create an employee",
-    "Create a role",
-    "Create a department",
-    "View employee",
-    "View role",
-    "View department",
-    "Update employee role"
+	"Create an employee",
+	"Create a role",
+	"Create a department",
+	"View employee",
+	"View role",
+	"View department",
+	"Update employee role"
 ];
 
 try {
-    cli();
+	cli();
 } catch (error) {
-    RTCPeerConnection.end();
+	connection.end();
 }
 
 async function cli() {
-    const connection = await mysql.createConnection({
-       host: "localhost",
-       user: "root",
-       database: "company_db" 
-    });
+	const connection = await mysql2.createConnection({
+        host: "localhost",
+        port: 9001,
+        user: "root",
+        password: "rootroot",
+		database: "company_db"
+	});
 
-    const {choice} = await inquirer.prompt([
-        {
-            type: "list",
-            name: "choice",
-            message: "First?",
-            choices: choiceOptions
-    }
-    // }, << if uncommented, delete yellow curly bracket on line above >> ---------- ReAd BeFoRe UnCoMmEnTiNg! <<-----{{{ <<-----{{{
-        // {
-        //     type: "list",
-        //     name: "secondQ",
-        //     message: "What type of employee would you like to add?",
-        //     choices: ["1", "2", "3", "4"]
-        // }
-    ])
+	const { choice } = await inquirer.prompt([{
+			type: "list",
+			name: "choice",
+			message: "What would you like to do?",
+			choices: choiceOptions
+		}
+		// }, << if uncommented, delete yellow curly bracket on line above >> ---------- ReAd BeFoRe UnCoMmEnTiNg! <<-----{{{ <<-----{{{
+		// {
+		//     type: "list",
+		//     name: "secondQ",
+		//     message: "What type of employee would you like to add?",
+		//     choices: ["1", "2", "3", "4"]
+		// }
+	])
 
-    switch (choice) {
-         
-        case "Create an employee": //first_name, last_name, role_id, manager_id
-            var {firstName} = await requestInput ("What is the new employee's first name", firstName);
-            var {lastName} = await requestInput ("What is the new employee's last name", lastName);
-            var {role_id} = await requestInput ("What is the new employee's role ID number", role_id);
-            var {manager_id} = await requestInput ("What is the new employee's manager ID number", manager_id);
-            console.log(first_name, last_name, role_id, manager_id);
-            cli();
-            break;
+	switch (choice) {
 
-        case "Create a role": //title, salary, department_id
-            var {title} = await requestInput ("What is the title for the new role?", title);
-            var {salary} = await requestInput ("What is the salary for the new role?", salary);
-            var {department_id} = await requestInput ("What is the department id number for the new role?", department_id);
-            console.log(title, salary, department_id);
-            cli();
-            break;
+		case "Create an employee": //first_name, last_name, role_id, manager_id
+			var {
+				firstName
+			} = await requestInput("What is the new employee's first name", firstName);
+			var {
+				lastName
+			} = await requestInput("What is the new employee's last name", lastName);
+			var {
+				role_id
+			} = await requestInput("What is the new employee's role ID number", role_id);
+			var {
+				manager_id
+			} = await requestInput("What is the new employee's manager ID number", manager_id);
+			console.log(first_name, last_name, role_id, manager_id);
+			cli();
+			break;
 
-        case "Create a department": //name
-            var {name} = await requestInput ("What is the name of the new department?", name);
-            console.log(name);
-            cli();
-            break;
+		case "Create a role": //title, salary, department_id
+			var {
+				title
+			} = await requestInput("What is the title for the new role?", title);
+			var {
+				salary
+			} = await requestInput("What is the salary for the new role?", salary);
+			var {
+				department_id
+			} = await requestInput("What is the department id number for the new role?", department_id);
+			console.log(title, salary, department_id);
+			cli();
+			break;
 
-        // case "View employee":
-        //     console.log(employeeSomething);
-        //     cli();
-        //     break;
+		case "Create a department": //name
+			var {
+				name
+			} = await requestInput("What is the name of the new department?", name);
+			console.log(name);
+			cli();
+			break;
 
-        // case "View role":
-        //     console.log(roleSomething);
-        //     cli();
-        //     break;
+			// case "View employee":
+			//     console.log(employeeSomething);
+			//     cli();
+			//     break;
 
-        // case "View department":
-        //     console.log(departmentSomething);
-        //     cli();
-        //     break;
+			// case "View role":
+			//     console.log(roleSomething);
+			//     cli();
+			//     break;
 
-        // case "Update employee role":
-        //     console.log(differentThing);
-        //     cli();
-        //     break;
+			// case "View department":
+			//     console.log(departmentSomething);
+			//     cli();
+			//     break;
 
-    }
-
-
-
-}
+			// case "Update employee role":
+			//     console.log(differentThing);
+			//     cli();
+			//     break;
+	}
+};
